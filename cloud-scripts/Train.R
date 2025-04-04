8ccd03560c3a9a191713de78446c472b465c0139
peter-silverstein <pesilver13@gmail.com>
2025-04-04 14:29:26 -0400

    added GCP files and restructuring

---
+++ cloud-scripts/Train.R
@@ -0,0 +1,36 @@
+# Loading libraries
+library(stan4bart)
+library(dbarts)
+library(here)
+library(bartCause)
+
+# Get environment variables set by Vertex AI
+model_dir <- Sys.getenv("AIP_MODEL_DIR", "")
+
+# Loading and preparing data
+df <- read_csv("train_data.csv")
+
+# Training the stan4bart model *****MAKE SURE TO FINALZIZE THIS DESIGN*****
+fit <- stan4bart(abs_dev ~ bart(.-g_weekday-g_hr-g_weekend-g_peak) + rapid_ride + (1 | weekday/hr),
+                   data = df,
+                   treatment = rapid_ride,
+                   cores = 2,
+                   chains = 8,
+                   iter = 2000,
+                   seed = 50,
+                   verbose = 2,
+                   bart_args = list(n.trees = 150))
+
+# Create a director if doesn't exist
+if (!dir.exists(model_dir)) {
+  dir.create(model_dir, recursive = TRUE)
+}
+
+# Save the model to specified directory
+# Handling an issue with saving stan4bart models
+fit@bart$fit@.xData$control <- fit@bart$fit@.xData$control[!names(fit@bart$fit@.xData$control) %in% c("callbacks")]
+
+# Saving the model
+save(fit, file = file.path(model_dir, "stan4bart_rapidride_model.RData"))
+
+cat("Model training completed and saved to", file.path(model_dir, "stan4bart_rapidride_model.RData"), "\n")
